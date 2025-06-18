e93c8d2c39f97c56f7d35b0d151c67485104b72a
Anushka Dilshan <anushkadilshan990@gmail.com>
2025-06-18 18:56:22 +0530

    add initial files and me related all server file as well

---
+++ backend/Movie Service/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//Routers
+
+
+//DB connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+        console.log("MongoDB connection successfull..!");
+        app.listen(process.env.PORT);
+        console.log(`Movie service Running on http://localhost:${process.env.MONGODB_URI}`);
+    })
+    .catch((error)=>{
+        console.log(error);
+        process.exit(1);
+    })
